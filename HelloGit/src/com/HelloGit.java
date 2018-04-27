fd9c0df1da9100da04ab1e33958b895c989e3f47
user40 <user40@gntp-40.gntp.local>
2018-04-27 14:45:24 +0900

    키보드로 이름 입력받기 추가

--- HelloGit/src/com/HelloGit.java
+++ HelloGit/src/com/HelloGit.java
@@ -1,9 +1,16 @@
 package com;
 
+import java.util.Scanner;
+
 public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.next();
+		
 
 	}
 
